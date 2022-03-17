cf9bf801a8745d22ec420fb2afbedb10a7505689
Chris Wood <chrispychris3@gmail.com>
2022-03-17 13:34:53 -0700

    Add Telescope to Neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,13 +1,22 @@
 local opts = { noremap = true, silent = true }
+local map = vim.api.nvim_set_keymap
 
--- Set leader key to space bar
+-- Set Neovim Escape.
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", opts)
+
+-- Set leader key to space bar.
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Set emmet leader key
+-- Set Emmet leader key.
 vim.g.user_emmet_leader_key = ","
 
--- Configure keymaps
+-- Set NERDTree key.
 vim.api.nvim_set_keymap("n", "<Leader>n", ":NERDTreeToggle<CR>", opts)
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", opts)
+
+-- Telescope autocommands.
+map("n", "<Leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
+map("n", "<Leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
+map("n", "<Leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", opts)
+map("n", "<Leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", opts)
--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -1,21 +1,33 @@
 local Plug = vim.fn["plug#"]
+local opts = { noremap = true, silent = true }
 
 vim.call("plug#begin", "/home/chris/.config/nvim/plugged")
-Plug("neovim/nvim-lspconfig")
-Plug("hrsh7th/nvim-cmp")
-Plug("hrsh7th/cmp-nvim-lsp")
-Plug("saadparwaiz1/cmp_luasnip")
-Plug("L3MON4D3/LuaSnip")
-Plug("scrooloose/nerdtree", {on = "NERDTreeToggle"})
-Plug("mattn/emmet-vim", {["for"] = {"css", "html"}})
-Plug("ellisonleao/gruvbox.nvim")
+  -- Neovim LSP and dependencies.
+  Plug("neovim/nvim-lspconfig")
+  Plug("hrsh7th/nvim-cmp")
+  Plug("hrsh7th/cmp-nvim-lsp")
+  Plug("saadparwaiz1/cmp_luasnip")
+  Plug("L3MON4D3/LuaSnip")
+
+  -- NERDTree.
+  Plug("scrooloose/nerdtree", {on = "NERDTreeToggle"})
+
+  -- Emmet.
+  Plug("mattn/emmet-vim", {["for"] = {"css", "html"}})
+
+  -- Neovim theme.
+  Plug("ellisonleao/gruvbox.nvim")
+
+  -- Telescope and dependencies.
+  Plug 'nvim-lua/plenary.nvim'
+  Plug 'nvim-telescope/telescope.nvim'
 vim.call("plug#end")
 
 
--- Load Gruvbox theme
+-- Load Gruvbox theme.
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
--- Emmet only for html/css
+-- Emmet only for html/css.
 vim.g.user_emmet_install_global = 0
 vim.api.nvim_exec([[ autocmd FileType html,css EmmetInstall]], false)
