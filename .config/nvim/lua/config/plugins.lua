20da3e8f5d4e1f9d146119410796c9c257979a78
Chris Wood <chrispychris3@gmail.com>
2022-03-18 17:23:54 -0700

    Add HTML5 snippet for emmet

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -31,3 +31,24 @@ vim.cmd([[colorscheme gruvbox]])
 -- Emmet only for html/css.
 vim.g.user_emmet_install_global = 0
 vim.api.nvim_exec([[ autocmd FileType html,css EmmetInstall]], false)
+vim.cmd [[let g:user_emmet_settings = {
+\  'variables': {'lang': 'en'},
+\  'html': {
+\    'default_attributes': {
+\      'option': {'value': v:null},
+\      'textarea': {'id': v:null, 'name': v:null, 'cols': 10, 'rows': 10},
+\    },
+\    'snippets': {
+\      'html:5': "<!DOCTYPE html>\n"
+\              ."<html lang=\"${lang}\">\n"
+\              ."<head>\n"
+\              ."\t<meta charset=\"${charset}\">\n"
+\              ."\t<title></title>\n"
+\              ."\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+\              ."</head>\n"
+\              ."<body>\n\t${child}|\n</body>\n"
+\              ."</html>",
+\    },
+\  },
+\}
+]]
