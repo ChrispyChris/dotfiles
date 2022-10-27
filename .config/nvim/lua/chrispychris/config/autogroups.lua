4d3efb80efe828d1ece1629b6f4ba69503946a39
Chris Wood <chrispychris3@gmail.com>
2022-10-27 00:48:53 -0700

    Add autogroup for nerdtree tmux-dev-server script

--- .config/nvim/lua/chrispychris/config/autogroups.lua
+++ .config/nvim/lua/chrispychris/config/autogroups.lua
@@ -4,3 +4,11 @@ local format_group = api.nvim_create_augroup("FormatGroup", { clear = true })
 api.nvim_create_autocmd(
     { "BufReadPost", "FileReadPost" },
     { pattern = "*", command = "normal zR", group = format_group })
+
+local nerdtree_group = api.nvim_create_augroup("NerdtreeGroup", { clear = true})
+api.nvim_create_autocmd(
+    { "StdinReadPre" },
+    { pattern = "*", command = "let s:std_in=1" })
+api.nvim_create_autocmd(
+    { "VimEnter" },
+    { pattern = "*", command = "if argc() == 0 && !exists('s:std_in') && v:this_session == '' | NERDTree", group = nerdtree_group })
