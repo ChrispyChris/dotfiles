edffd93fec8c1aa42d7d9ab7506f14ca094d58e4
Chris Wood <chrispychris3@gmail.com>
2024-05-20 19:37:23 -0700

    Update neovim configuration and plugins.

--- .config/nvim/lua/chrispychris/config/keymaps.lua
+++ .config/nvim/lua/chrispychris/config/keymaps.lua
@@ -5,17 +5,23 @@ end
 local create_autocmd = vim.api.nvim_create_autocmd
 local create_augroup = vim.api.nvim_create_augroup
 
--- Set Neovim Escape.
-map("i", "jj", "<Esc>", opts)
-
--- Set Control+D/U to center the cursor vertically in the buffer on move.
-map("n", "<C-d>", "<C-d>zz", opts)
-map("n", "<C-u>", "<C-u>zz", opts)
-
 -- Set leader key to space bar.
 map("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Set Neovim Escape.
+map("i", "jj", "<Esc>", opts)
+
+-- Keybinds to make split navigation easier.
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Set Control+D/U to center the cursor vertically in the buffer on move.
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
 
 -- Highlight when yanking text.
 create_autocmd("TextYankPost", {
@@ -25,12 +31,11 @@ create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+-- Clear highlight from searching when pressing <Esc> in normal mode.
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Set Emmet leader key.
 vim.g.user_emmet_leader_key = ","
-
--- Set NERDTree key.
--- map("n", "<Leader>n", "<cmd>NERDTreeToggle<CR>", opts)
 
 -- Telescope autocommands.
 map("n", "<Leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
--- .config/nvim/lua/chrispychris/plugins/nerdtree.lua
+++ .config/nvim/lua/chrispychris/plugins/nerdtree.lua
@@ -3,6 +3,7 @@ return {
     "preservim/nerdtree",
     lazy = true,
     keys = { { "<leader>n", "<cmd>NERDTreeToggle<cr>", desc = "Toggle NERDTree." } },
+    cmd = { "NERDTree" },
     opts = { },
   }
 }
