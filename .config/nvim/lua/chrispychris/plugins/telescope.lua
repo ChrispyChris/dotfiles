dea88a5a7b5d2bf30ef3525d00233df7aaf38afb
Chris Wood <chrispychris3@gmail.com>
2024-06-01 10:18:00 -0700

    Update neovim configs.

--- .config/nvim/lua/chrispychris/plugins/telescope.lua
+++ .config/nvim/lua/chrispychris/plugins/telescope.lua
@@ -1,33 +1,38 @@
+local map = require("chrispychris.utils.keymap").map
+
 return {
-  'nvim-telescope/telescope.nvim', branch = '0.1.x',
+  "nvim-telescope/telescope.nvim", branch = "0.1.x",
   dependencies = { 
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
     {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
       cond = function()
-        return vim.fn.executable 'make' == 1
+        return vim.fn.executable "make" == 1
       end,
     },
     { 
-      'nvim-tree/nvim-web-devicons',
+      "nvim-tree/nvim-web-devicons",
       enabled = vim.g.have_nerd_font,
     },
   },
   config = function()
-    require('telescope').setup {
+    require("telescope").setup {
       defaults = {
         mappings = {
-          i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         },
       },
     }
-    pcall(require('telescope').load_extension, 'fzf')
+    pcall(require("telescope").load_extension, "fzf")
+
     -- Telescope autocommands.
---    local builtin = require "telescope.builtin"
---    map("n", "<Leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", { desc = "Find files" })
---    map("n", "<Leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", { desc = "Live grep" })
---    map("n", "<Leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", { desc = "Search buffers" })
---    map("n", "<Leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", { desc = "Search help tags" })
+    map("n", "<Leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", { desc = "Find files" })
+    map("n", "<Leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", { desc = "Live grep" })
+    map("n", "<Leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", { desc = "Search buffers" })
+    map("n", "<Leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", { desc = "Search help tags" })
+    map("n", "<leader>sw", "<cmd>lua require('telescope.builtin').grep_string()<CR>", { desc = "Search current word" })
+    map("n", "<leader>sd", "<cmd>lua require('telescope.builtin').diagnostics()<CR>", { desc = "Search diagnostics" })
+    map("n", "<leader>cb", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>", { desc = "Search current buffer fuzzily" })
+    map("n", "<leader>sc", "<cmd>lua require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') })<CR>", { desc = "Search Neovim configuration files" })
   end,
 }
